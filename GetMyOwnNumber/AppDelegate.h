169dceb1e4f8e3fadf6f4b5abcf79d901f892402
GL <1247759034@qq.com>
2016-05-09 17:33:18 +0800

    Initial Commit

---
+++ GetMyOwnNumber/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GetMyOwnNumber
+//
+//  Created by 66 on 16/5/9.
+//  Copyright © 2016年 66. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GetMyOwnNumber/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GetMyOwnNumber
+//
+//  Created by 66 on 16/5/9.
+//  Copyright © 2016年 66. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
